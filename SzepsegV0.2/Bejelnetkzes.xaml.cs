7568eaf259d5dfca605bfeb839d01b9779c6bce2
gyarmati.csanad <20.gyarmati.csanad@mechwart.com>
2024-10-02 13:51:03 +0200

    dominiknak a rossz pusholasanak javitasa

--- SzepsegV0.2/Bejelnetkzes.xaml.cs
+++ SzepsegV0.2/Bejelnetkzes.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using MySql.Data.MySqlClient;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,18 +7,13 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace SzepsegV0._2
 {
-    /// <summary>
-    /// Interaction logic for Bejelnetkzes.xaml
-    /// </summary>
-    public partial class Bejelnetkzes : Window
+    public partial class MainWindow : Window
     {
-        public Bejelnetkzes()
-        {
-            InitializeComponent();
-        }
     }
 }
