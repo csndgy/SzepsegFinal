656dcb635a47a0b2ad5a6077af9cc34f9ecaead3
gyarmati.csanad <20.gyarmati.csanad@mechwart.com>
2024-10-04 11:33:44 +0200

    main ablak (bejelentkezes neven) letrehozva, idopont fogalas ablak nem nyithato meg tobbszor mint 1

--- SzepsegV0.2/Bejelnetkzes.xaml.cs
+++ SzepsegV0.2/Bejelnetkzes.xaml.cs
@@ -15,5 +15,25 @@ namespace SzepsegV0._2
 {
     public partial class Bejelnetkzes : Window
     {
+        private MainWindow bejelentkezesAblak;
+
+        public Bejelnetkzes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFoglalas_Click(object sender, RoutedEventArgs e)
+        {
+            if (bejelentkezesAblak == null || !bejelentkezesAblak.IsVisible)
+            {
+                bejelentkezesAblak = new MainWindow();
+                bejelentkezesAblak.Closed += (s, args) => bejelentkezesAblak = null; //??? chatG
+                bejelentkezesAblak.Show();
+            }
+            else
+            {
+                bejelentkezesAblak.Activate();
+            }
+        }
     }
 }
